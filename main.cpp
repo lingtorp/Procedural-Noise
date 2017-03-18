19586c1c9893f6fac12fb64d8496a70c369899fd
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2017-03-18 16:19:36 +0100

    Animated the 3D noise for greater visual effect!

--- main.cpp
+++ main.cpp
@@ -2,21 +2,18 @@
 #include "noise.hpp"
 #include <SDL2/SDL.h>
 
-int main() {
-    int WIDTH = 1000;
-    int HEIGHT = 1000;
-    int ZOOM = 1;
-    int DIVISOR = 2;
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
-    auto noise_gen = Perlin(1);
-    bool quit;
-    SDL_Event event;
+const int WIDTH = 1000;
+const int HEIGHT = 1000;
+const int ZOOM = 1;
+const int DIVISOR = 2;
+const int SEED = 1;
+const double TIME_STEP = 0.01;
+
+void draw(double time, SDL_Renderer *renderer, Noise &noise_gen) {
     for (size_t x = 0; x < WIDTH; x += ZOOM) {
         for (size_t y = 0; y < HEIGHT; y += ZOOM) {
-            // auto noise = noise_gen.octaves(x / DIVISOR, y / DIVISOR, 4, 0.5);
-            auto noise = noise_gen.get_value(x / DIVISOR, y / DIVISOR, 2.123);
+            // auto noise = noise_gen.octaves(x / DIVISOR, y / DIVISOR, 8, 0.5);
+            auto noise = noise_gen.get_value(x / DIVISOR, y / DIVISOR, time / DIVISOR);
             auto color = 125.5f + noise * 125.5f;
             // SDL_Log("Noise: %f, Color: %f \n", noise, color);
             SDL_SetRenderDrawColor(renderer, color, color, color, 1.0f);
@@ -24,12 +21,23 @@ int main() {
         }
     }
     SDL_RenderPresent(renderer);
+}
 
+int main() {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
+    Perlin noise(SEED);
+    bool quit = false;
+    double time = 0.0;
+    SDL_Event event;
     while (!quit) {
         if (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT)
-                break;
+                quit = true;
         }
+        time += TIME_STEP;
+        draw(time, renderer, noise);
     }
 
     SDL_DestroyRenderer(renderer);
