7a32c6c1bd78ae736891f142ee115bf21ac8cdc0
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2017-04-21 18:24:18 +0200

    Testing Simplex_Patent

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ const double TIME_STEP = 0.5;
 void draw(double time, SDL_Renderer *renderer, Noise &noise_gen) {
     for (size_t x = 0; x < WIDTH; x += ZOOM) {
         for (size_t y = 0; y < HEIGHT; y += ZOOM) {
-            std::vector<float> amplitudes = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
-            auto noise = noise_gen.octaves(x, y, time, amplitudes);
+            // std::vector<float> amplitudes = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+            // auto noise = noise_gen.octaves(x, y, time, amplitudes);
             // auto noise = std::abs(noise_gen.octaves(x, y, time, DIVISOR, 0.9));
             // auto noise = noise_gen.get_value(x / DIVISOR, y / DIVISOR);
             // auto noise = noise_gen.get_value(x / DIVISOR, y / DIVISOR, time / DIVISOR);
@@ -21,6 +21,7 @@ void draw(double time, SDL_Renderer *renderer, Noise &noise_gen) {
             // auto noise = noise_gen.domain_wrapping(x, y, time, DIVISOR);
             // auto noise = noise_gen.turbulence_ridged(x, y, time, DIVISOR);
             // auto noise = noise_gen.get_value(x, y);
+            auto noise = noise_gen.turbulence(x, y, DIVISOR);
             auto color = 125.5f + noise * 125.5f;
             // SDL_Log("Noise: %f, Color: %f \n", noise, color);
             SDL_SetRenderDrawColor(renderer, color, color, color, 1.0f);
@@ -34,7 +35,7 @@ int main() {
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
-    Perlin noise(SEED);
+    Simplex_Patent noise(SEED);
     bool quit = false;
     double time = 0.0;
     auto last_tick = SDL_GetTicks();
