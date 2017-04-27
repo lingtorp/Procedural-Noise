7094a85b41241a7656f9633acf83bca2e7bdeecb
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2017-04-27 12:03:47 +0200

    Added the improved Perlin noise algorithm from 2002
    It removes the huge gradient table for a smaller one by using the
    entropy from the permutation table. There is also the new interpolation
    function.

--- noise.hpp
+++ noise.hpp
@@ -484,7 +484,182 @@ public:
     double get_value(double x, double y, double z) const override { exit(EXIT_FAILURE); }
 };
 
-/// Standard Perlin noise implementation
+/**
+ * Improved Perlin noise from 2002
+ * ACM: http://dl.acm.org/citation.cfm?id=566636
+ *
+ * The improvements made by Kenneth Perlin were twofold;
+ * 1) Randomly generated gradients changed to static gradients
+ * 2) Changed interpolation function
+ */
+class Perlin_Improved : public Noise {
+    // Implementation details for generation of gradients
+    std::mt19937 engine;
+    std::uniform_real_distribution<> distr;
+
+    /// 2D Normalized gradients table
+    std::vector<Vec2<double>> grads;
+
+    /// 3D Normalized gradients table
+    std::vector<Vec3<double>> grads3;
+
+    /// Permutation table for indices to the gradients (2D)
+    std::vector<u_char> perms;
+
+    /// Permutation table for indices to the gradients (3D)
+    std::vector<u_char> perms3;
+
+public:
+    Perlin_Improved(uint64_t seed): engine(seed), grads(4), grads3(16), distr(-1.0, 1.0), perms(256), perms3(256) {
+        /// 4 gradients for each edge of a unit square, no need for padding, is power of 2
+        grads = {
+                {1, 0},
+                {0, 1},
+                {-1, 0},
+                {0, -1}
+        };
+
+        /// 12 gradients from the center to each edge of a unit cube, 4 duplicated vectors for padding so that the modulo is on a power of 2 (faster)
+        grads3 = {
+                {1, 1, 0},
+                {-1, 1, 0},
+                {1, -1, 0},
+                {-1, -1, 0},
+                {1, 0, 1},
+                {-1, 0, 1},
+                {1, 0, -1},
+                {-1, 0, -1},
+                {0, 1, 1},
+                {0, -1, 1},
+                {0, 1, -1},
+                {0, -1, -1},
+                {1, 1, 0},
+                {-1, 1, 0},
+                {0, -1, 1},
+                {0, -1, -1}
+        };
+
+        /// Fill gradient lookup array with random indices to the gradients list
+        for (int i = 0; i < perms.size(); i++) { perms[i] = i % grads.size(); }
+        for (int i = 0; i < perms3.size(); i++) { perms3[i] = i % grads3.size(); }
+
+        /// Randomize the order of the indices
+        std::shuffle(perms.begin(), perms.end(), engine);
+    }
+
+    double get_value(double X, double Y) const {
+        /// Compress the coordinates inside the chunk; double part + int part = point coordinate
+        X += 0.1; Y += 0.1; // Skew coordinates to avoid integer lines becoming zero
+        /// Grid points from the chunk in the world
+        int X0 = (int) std::floor(X);
+        int Y0 = (int) std::floor(Y);
+        int X1 = (int) std::ceil(X);
+        int Y1 = (int) std::ceil(Y);
+
+        /// Gradients using hashed indices from lookup list
+        Vec2<double> x0y0 = grads[perms[(X0 + perms[Y0 % perms.size()]) % perms.size()]];
+        Vec2<double> x1y0 = grads[perms[(X1 + perms[Y0 % perms.size()]) % perms.size()]];
+        Vec2<double> x0y1 = grads[perms[(X0 + perms[Y1 % perms.size()]) % perms.size()]];
+        Vec2<double> x1y1 = grads[perms[(X1 + perms[Y1 % perms.size()]) % perms.size()]];
+
+        /// Vectors from gradients to point in unit square
+        auto v00 = Vec2<double>{X - X0, Y - Y0};
+        auto v10 = Vec2<double>{X - X1, Y - Y0};
+        auto v01 = Vec2<double>{X - X0, Y - Y1};
+        auto v11 = Vec2<double>{X - X1, Y - Y1};
+
+        /// Contribution of gradient vectors by dot product between relative vectors and gradients
+        double d00 = x0y0.dot(v00);
+        double d10 = x1y0.dot(v10);
+        double d01 = x0y1.dot(v01);
+        double d11 = x1y1.dot(v11);
+
+        /// Interpolate dot product values at sample point using polynomial interpolation 6x^5 - 15x^4 + 10x^3
+        double yf = Y - Y0; // Float offset inside the square [0, 1]
+        double xf = X - X0; // Float offset inside the square [0, 1]
+
+        auto wx = fade(xf);
+        auto wy = fade(yf);
+
+        /// Interpolate along x for the contributions from each of the gradients
+        auto xa = lerp(wx, d00, d10);
+        auto xb = lerp(wx, d01, d11);
+
+        auto val = lerp(wy, xa, xb);
+
+        return val;
+    }
+
+    double get_value(double X, double Y, double Z) const {
+        /// Compress the coordinates inside the chunk; double part + int part = point coordinate
+        /// Grid points from the chunk in the world
+        int X0 = (int) std::floor(X);
+        int Y0 = (int) std::floor(Y);
+        int X1 = (int) std::ceil(X);
+        int Y1 = (int) std::ceil(Y);
+        int Z0 = (int) std::floor(Z);
+        int Z1 = (int) std::ceil(Z);
+
+        /// Gradients using hashed indices from lookup list
+        Vec3<double> x0y0z0 = grads3[perms[(X0 + perms[(Y0 + perms[Z0 % perms.size()]) % perms.size()]) % perms.size()]];
+        Vec3<double> x1y0z0 = grads3[perms[(X1 + perms[(Y0 + perms[Z0 % perms.size()]) % perms.size()]) % perms.size()]];
+        Vec3<double> x0y1z0 = grads3[perms[(X0 + perms[(Y1 + perms[Z0 % perms.size()]) % perms.size()]) % perms.size()]];
+        Vec3<double> x1y1z0 = grads3[perms[(X1 + perms[(Y1 + perms[Z0 % perms.size()]) % perms.size()]) % perms.size()]];
+
+        Vec3<double> x0y0z1 = grads3[perms[(X0 + perms[(Y0 + perms[Z1 % perms.size()]) % perms.size()]) % perms.size()]];
+        Vec3<double> x1y0z1 = grads3[perms[(X1 + perms[(Y0 + perms[Z1 % perms.size()]) % perms.size()]) % perms.size()]];
+        Vec3<double> x0y1z1 = grads3[perms[(X0 + perms[(Y1 + perms[Z1 % perms.size()]) % perms.size()]) % perms.size()]];
+        Vec3<double> x1y1z1 = grads3[perms[(X1 + perms[(Y1 + perms[Z1 % perms.size()]) % perms.size()]) % perms.size()]];
+
+        /// Vectors from gradients to point in unit cube
+        auto v000 = Vec3<double>{X - X0, Y - Y0, Z - Z0};
+        auto v100 = Vec3<double>{X - X1, Y - Y0, Z - Z0};
+        auto v010 = Vec3<double>{X - X0, Y - Y1, Z - Z0};
+        auto v110 = Vec3<double>{X - X1, Y - Y1, Z - Z0};
+
+        auto v001 = Vec3<double>{X - X0, Y - Y0, Z - Z1};
+        auto v101 = Vec3<double>{X - X1, Y - Y0, Z - Z1};
+        auto v011 = Vec3<double>{X - X0, Y - Y1, Z - Z1};
+        auto v111 = Vec3<double>{X - X1, Y - Y1, Z - Z1};
+
+        /// Contribution of gradient vectors by dot product between relative vectors and gradients
+        double d000 = x0y0z0.dot(v000);
+        double d100 = x1y0z0.dot(v100);
+        double d010 = x0y1z0.dot(v010);
+        double d110 = x1y1z0.dot(v110);
+
+        double d001 = x0y0z1.dot(v001);
+        double d101 = x1y0z1.dot(v101);
+        double d011 = x0y1z1.dot(v011);
+        double d111 = x1y1z1.dot(v111);
+
+        /// Interpolate dot product values at sample point using polynomial interpolation 6x^5 - 15x^4 + 10x^3
+        double yf = Y - Y0; // Float offset inside the cube [0, 1]
+        double xf = X - X0; // Float offset inside the cube [0, 1]
+        double zf = Z - Z0; // Float offset inside the cube [0, 1]
+
+        auto wx = fade(xf);
+        auto wy = fade(yf);
+        auto wz = fade(zf);
+
+        /// Interpolate along x for the contributions from each of the gradients
+        auto xa = lerp(wx, d000, d100);
+        auto xb = lerp(wx, d010, d110);
+
+        auto xc = lerp(wx, d001, d101);
+        auto xd = lerp(wx, d011, d111);
+
+        /// Interpolate along y for the contributions from each of the gradients
+        auto ya = lerp(wy, xa, xb);
+        auto yb = lerp(wy, xc, xd);
+
+        /// Interpolate along z for the contributions from each of the gradients
+        auto za = lerp(wz, ya, yb);
+
+        return za;
+    }
+};
+
 class Perlin : public Noise {
     // Implementation details for generation of gradients
     std::mt19937 engine;
