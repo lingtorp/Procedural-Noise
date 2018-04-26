8f45ee9a124b19d0a8ae02dd22a918a1b90fa8a1
Alexander Lingtorp <AlexanderLingtorp@Alexanders-MacBook-Pro.fritz.box>
2018-04-26 11:17:25 +0200

    Minor changes to vector
    Will refactor this into its own gitmodule in order to have some
    consistency across projects.

--- vector.hpp
+++ vector.hpp
@@ -1,31 +1,22 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-/**
- * A collection of linear algebra data types mainly vectors and their operations
- */
-
 #include <math.h>
 #include <ostream>
 #include <cmath>
 #include <vector>
 
-/// Forward declarations
-template<typename T>
-struct Vec2;
-
-template<typename T>
-struct Vec3;
+// TODO: Add Doxygen style comments and documentation
+/**
+ * A collection of linear algebra data types mainly vectors and some of their operations.
+ */
 
 template<typename T>
 struct Vec3 {
     T x, y, z;
 
     Vec3(T x, T y, T z): x(x), y(y), z(z) {};
-    Vec3(): x(0), y(0), z(0) {};
-
-    /// Vec3 of zeroes
-    inline static Vec3 ZERO() { return Vec3(0.0, 0.0, 0.0); }
+    Vec3(): x(0.0), y(0.0), z(0.0) {};
 
     /// Length of the vector
     inline double length() const { return std::sqrt(std::pow(x, 2) + std::pow(y, 2) + std::pow(z, 2)); }
@@ -59,32 +50,20 @@ struct Vec3 {
     inline T dot(Vec3<T> u) const { return x * u.x + y * u.y + z * u.z; }
 
     /// Operators
-    inline Vec3<T> operator+(const Vec3 &rhs) const {
-        return Vec3<T>{x + rhs.x, y + rhs.y, z + rhs.z};
-    }
+    inline Vec3<T> operator+(const Vec3 &rhs) const { return Vec3<T>{x + rhs.x, y + rhs.y, z + rhs.z}; }
 
-    inline Vec3<T> operator+(const double rhs) const {
-        return Vec3<T>{x + rhs, y + rhs, z + rhs};
-    }
+    inline Vec3<T> operator+(const double rhs) const { return Vec3<T>{x + rhs, y + rhs, z + rhs}; }
 
-    inline Vec3<T> operator*(const Vec3 &rhs) const {
-        return Vec3<T>{x * rhs.x, y * rhs.y, z * rhs.z};
-    }
+    inline Vec3<T> operator*(const Vec3 &rhs) const { return Vec3<T>{x * rhs.x, y * rhs.y, z * rhs.z}; }
 
-    inline Vec3<T> operator*(const T s) const {
-        return Vec3<T>{x * s, y * s, z * s};
-    }
+    inline Vec3<T> operator*(const T s) const { return Vec3<T>{x * s, y * s, z * s}; }
 
-    inline bool operator==(const Vec3 &rhs) const {
-        return (x == rhs.x) && (y == rhs.y) && (z == rhs.z);
-    }
-
+    inline bool operator==(const Vec3 &rhs) const { return (x == rhs.x) && (y == rhs.y) && (z == rhs.z); }
+    
+    inline Vec3 operator-(const Vec3 &rhs) const { return Vec3{x - rhs.x, y - rhs.y, z - rhs.z}; }
+    
     friend std::ostream &operator<<(std::ostream& os, const Vec3 &vec) {
         return os << "(x:" << vec.x << " y:" << vec.y << " z:" << vec.z << ")";
-    }
-
-    inline Vec3 operator-(const Vec3 &rhs) const {
-        return Vec3{x - rhs.x, y - rhs.y, z - rhs.z};
     }
 };
 
@@ -92,7 +71,7 @@ template<typename T>
 struct Vec2 {
     T x, y = 0.0f;
     Vec2(T x, T y): x(x), y(y) {};
-    Vec2(): x(0), y(0) {};
+    Vec2(): x(0.0), y(0.0) {};
 
     /// Sum of the components of the vector
     inline T sum() const { return x + y; }
@@ -104,17 +83,11 @@ struct Vec2 {
     inline T dot(Vec2<T> u) const { return x * u.x + y * u.y; }
 
     /// Operators
-    Vec2<T> operator+(const Vec2 &rhs) const {
-        return {x + rhs.x, y + rhs.y};
-    }
+    Vec2<T> operator+(const Vec2 &rhs) const { return {x + rhs.x, y + rhs.y}; }
 
-    Vec2<T> operator-(const Vec2 &rhs) const {
-        return {x - rhs.x, y - rhs.y};
-    }
+    Vec2<T> operator-(const Vec2 &rhs) const { return {x - rhs.x, y - rhs.y}; }
 
-    bool operator==(const Vec2 &rhs) const {
-        return x == rhs.x && y == rhs.y;
-    }
+    bool operator==(const Vec2 &rhs) const { return x == rhs.x && y == rhs.y; }
 
     /// Returns a copy of this vector normalized
     inline Vec2<T> normalize() const {
